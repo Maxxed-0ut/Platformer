ced6fd6e9a8d1c0143af599210b894b520096081
Maxxed-0ut <shebantih@gmail.com>
2025-08-02 12:21:10 +0530

    Event Script(Fast)

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,11 +1,29 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
 
 public abstract class Interactable : MonoBehaviour
 {
-
+    public bool useEvents;
+    [SerializeField]
     public string promptMessage;
+
+    public virtual string OnLook()
+    {
+        return promptMessage;
+    }
     public void BaseInteract()
     {
+        if (useEvents)
+        {
+            InteractionEvent interactionEvent = GetComponent<InteractionEvent>();
+            if (interactionEvent != null && interactionEvent.OnInteract != null)
+            {
+                interactionEvent.OnInteract.Invoke();
+            }
+        }
+
         Interact();
     }
     protected virtual void Interact()
